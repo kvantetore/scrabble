eeb96d9e67e11642762917d7fe37f274f3f05fa7
Tore <tore@norbits.no>
2015-06-08 10:02:00 +0200

    Added angular2 router and some rudimentary routes

---
+++ src/home/home.ts
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: "home"
+})
+@View({
+  templateUrl: "home/home.html"
+})
+export class HomeComponent {
+
+}
---
+++ src/newGame/newGame.ts
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: "new-game"
+})
+@View({
+  templateUrl: "newGame/newGame.html"
+})
+export class NewGameComponent {
+
+}
