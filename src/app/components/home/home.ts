11022fe5c0b7fa1dd0e78bd4d37dd60d652c23bc
Tore <tore@norbits.no>
2015-06-17 22:29:36 +0200

    Improved style of game list on home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,7 +2,9 @@ import {Component, View, coreDirectives, ElementRef} from 'angular2/angular2';
 import {RouterLink, RouteConfig, Router} from 'angular2/router';
 
 import {Game} from 'app/models/game';
+import {Player} from 'app/models/player';
 import {GameService} from 'app/api/gameService';
+import {PlayerService} from 'app/api/playerService';
 
 let styles   = require('!raw!less!./home.less');
 let template = require('./home.html');
@@ -16,8 +18,14 @@ let template = require('./home.html');
 })
 export class HomeComponent {
   games: Game[];
+  players: Player[];
 
-  constructor(gameService: GameService, private router: Router) {
+  constructor(gameService: GameService, playerService: PlayerService, private router: Router) {
+    playerService.listPlayers()
+      .then(players => {
+        this.players = players;
+      });
+    
     gameService.listActiveGames()
       .then(games => {
         this.games = games;
@@ -26,6 +34,14 @@ export class HomeComponent {
   
   gameUrl(game: Game) {
     return this.router.parent.generate("play-game", {id: game.id});
+  }
+  
+  getGamePlayers(game: Game) {
+    if (this.players == null) {
+      return null;
+    }
+    
+    return game.playerIds.map(pid => this.players.find(p => p.id === pid));
   }
   
   gotoGame($event: any,game: Game) {
